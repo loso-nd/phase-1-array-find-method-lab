56b2a7230128f05b3b0a92cf3407ea263ce0f403
Nahum <losond8@gmail.com>
2021-02-25 13:55:04 -0800

    commit tree cf3e19d74eaaec212a36f4e69a2e2d8cd8b0b538

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
+// declare function with 1 param
+// The test has an arg called record 
+// passing find with a callback func which iterates thru the array to find
+// what we are looking for.
 
+const superbowlWin = (potato) => {
+    const check = potato.find(element => element.result === "W");
+    if (check) {
+        return check["year"]
+    }
+    // return check["year"]   
+}
+
+
+
+// var foundValue = array.filter(obj=>obj.name==='string 1');
+
+// let obj = arr.find(o => o.name === 'string 1');
