d39b9b6f847272916618a835190a962d17bfca90
Nahum <losond8@gmail.com>
2021-02-25 14:01:23 -0800

    Find method searches for the object and returns true or false

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
+// declare function with 1 param
+// The test has an arg called record 
+// passing find with a callback func which iterates thru the array to find
+// what we are looking for.
+
+const superbowlWin = (potato) => {
+    const check = potato.find(element => element.result === "W"); // returns true or false
+    if (check) {
+        return check["year"]
+    }
+
+    // return check["year"] -- alone returns year via [] notation
+}
 
